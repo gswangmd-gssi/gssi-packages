4fe58949d940de1f0c15627781f8b4842c6deb22
gswangmd-gssi <gssiwmd@gmail.com>
2021-11-18 21:40:52 +0800

    modified:   luci-app-adguardhome-wmd/luasrc/model/cbi/AdGuardHome/base.lua

--- luci-app-adguardhome-wmd/luasrc/model/cbi/AdGuardHome/base.lua
+++ luci-app-adguardhome-wmd/luasrc/model/cbi/AdGuardHome/base.lua
@@ -63,9 +63,7 @@ if (port == "") then port = "?" end
 o = s:option(ListValue, "redirect", port .. translate("Redirect"), translate("AdGuardHome redirect mode"))
 o.placeholder = "none"
 o:value("none", translate("none"))
-o:value("dnsmasq-upstream", translate("Run as dnsmasq upstream server"))
 o:value("redirect", translate("Redirect 53 port to AdGuardHome"))
-o:value("exchange", translate("Use port 53 replace dnsmasq"))
 o.default = "none"
 o.optional = true
 
